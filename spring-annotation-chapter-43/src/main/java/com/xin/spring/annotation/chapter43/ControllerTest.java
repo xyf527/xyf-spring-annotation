41ef48b89eb90dc909c7b994125c3890fcf6db51
XYF527 <XYF527@noreply.gitcode.com>
2025-12-28 13:36:21 +0800

    day43-finish

---
+++ spring-annotation-chapter-43/src/main/java/com/xin/spring/annotation/chapter43/ControllerTest.java
@@ -0,0 +1,16 @@
+package com.xin.spring.annotation.chapter43;
+
+import com.xin.spring.annotation.springmvc.starter.TomcatStarter;
+
+/**
+ * @author XYF
+ * @date 2025-12-27 15:19
+ */
+
+public class ControllerTest {
+
+    public static void main(String[] args) {
+        TomcatStarter.start(8888);
+    }
+
+}
---
+++ spring-annotation-chapter-43/src/main/java/com/xin/spring/annotation/chapter43/controller/XinController.java
@@ -0,0 +1,13 @@
+package com.xin.spring.annotation.chapter43.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author XYF
+ * @date 2025-12-27 15:19
+ */
+
+@Controller
+public class XinController {
+
+}
