3b722c14610463a989b8e40930f10cfa8222d1ca
XYF527 <XYF527@noreply.gitcode.com>
2025-12-28 13:54:50 +0800

    day44-finish

---
+++ spring-annotation-chapter-44/src/main/java/com/xin/spring/annotation/chapter44/RestControllerTest.java
@@ -0,0 +1,16 @@
+package com.xin.spring.annotation.chapter44;
+
+import com.xin.spring.annotation.springmvc.starter.TomcatStarter;
+
+/**
+ * @author XYF
+ * @date 2025-12-28 13:44
+ */
+
+public class RestControllerTest {
+
+    public static void main(String[] args) {
+        TomcatStarter.start(8888);
+    }
+
+}
---
+++ spring-annotation-chapter-44/src/main/java/com/xin/spring/annotation/chapter44/controller/XinController.java
@@ -0,0 +1,13 @@
+package com.xin.spring.annotation.chapter44.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author XYF
+ * @date 2025-12-28 13:44
+ */
+
+@RestController
+public class XinController {
+
+}
